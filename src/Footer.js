cd95d1277e4326a4f776e015c38d4d22519fc3eb
Grant <grantyli94@gmail.com>
2021-10-06 23:43:09 -0400

    added Email in Footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,16 @@
 const GITHUB = "https://github.com/grantyli94";
 const LINKEDIN = "https://www.linkedin.com/in/grant-li-b097a66b/";
+const EMAIL = "mailto:grantyli94@gmail.com"
 
 function Footer() {
 
   return (
     <div className="Footer">
       <div className="Footer-Text">
-        <span><a href={GITHUB}>GitHub</a> | <a href={LINKEDIN}>LinkedIn</a></span>
+        <span>
+          <a href={GITHUB}>GitHub</a> | 
+          <a href={LINKEDIN}> LinkedIn</a> | 
+          <a href={EMAIL}> Email</a></span>
       </div>
     </div>
   );
