3e6bc8db2d87bca33827e12bedf23d2d84237953
Grant <grantyli94@gmail.com>
2021-10-09 18:04:19 -0400

    added aim.ly to project list

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,21 +2,24 @@ import ProjectCard from "./ProjectCard";
 import jobly from './jobly.png';
 import warbler from './warbler.png';
 import minesweeper from './minesweeper.png';
+import aimly from './aimly.png';
 
 const JOBLY_URL = "http://jobly-grant-li.surge.sh/";
 const WARBLER_URL = "https://warbler-r22-grant-li.herokuapp.com/";
 const MINESWEEPER_URL = "http://www.minesweeper.grant-li.com/";
+const AIMLY_URL = "https://aimly.surge.sh/";
 
 const joblyText = (
   <div>
-    <p><b>Tech Stack:</b> React, Express, PostgreSQL, Bootstrap</p>
-    <p><b>Description:</b> Fullstack <i>LinkedIn</i> clone where users can view and apply to company job listings</p>
+    <p><b>Tech Stack:</b> React, Express, PostgreSQL</p>
+    <p><b>Description:</b> Fullstack LinkedIn clone where users can view and apply to company job listings</p>
   </div>
 );
+
 const warblerText = (
   <div>
     <p><b>Tech Stack:</b> Flask, Jinja, SQLAlchemy, PostgreSQL</p>
-    <p><b>Description:</b> Backend <i>Twitter</i> clone where users can follow and message other users</p>
+    <p><b>Description:</b> Backend Twitter clone where users can follow and message other users</p>
   </div>
 );
 
@@ -24,6 +27,13 @@ const minesweeperText = (
   <div>
     <p><b>Tech Stack:</b> React, TypeScript</p>
     <p><b>Description:</b> Based off of the popular Minesweeper game; built with a depth-first search (DFS) algorithm</p>
+  </div>
+);
+
+const aimlyText = (
+  <div>
+    <p><b>Tech Stack:</b> React, Express, PostgreSQL</p>
+    <p><b>Description:</b> Light-weight first-person shooter (FPS) aim trainer</p>
   </div>
 );
 
@@ -36,6 +46,7 @@ function ProjectList() {
         <ProjectCard name="Jobly" img={jobly} url={JOBLY_URL} text={joblyText}/>
         <ProjectCard name="Warbler" img={warbler} url={WARBLER_URL} text={warblerText}/>
         <ProjectCard name="Minesweeper" img={minesweeper} url={MINESWEEPER_URL} text={minesweeperText}/>
+        <ProjectCard name="Aim.ly" img={aimly} url={AIMLY_URL} text={aimlyText}/>
       </div>
     </div>
   );
