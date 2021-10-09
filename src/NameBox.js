e52f2ec063cc65801614c4edb201c88e90073cdd
Grant <grantyli94@gmail.com>
2021-10-09 18:05:24 -0400

    changed NameBox text

--- src/NameBox.js
+++ src/NameBox.js
@@ -4,7 +4,7 @@ function NameBox() {
     <div className="NameBox">
       <div className="NameBox-Text">
         <h1>Grant Li</h1>
-        <p>An aspiring software developer from a finance background</p>
+        <p>A software developer from a finance background</p>
       </div>
     </div>
   );
